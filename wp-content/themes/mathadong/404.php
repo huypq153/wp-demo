052e9c86f154c08cc9988a20490da82644dbc387
huypq153 <phamquanghuy83@gmail.com>
2017-05-17 18:17:44 +0700

    test 1

---
+++ wp-content/themes/mathadong/404.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ */
+
+get_header(); ?>
+<?php get_template_part('template-parts/content','page-head')?>	
+<div class="page-content" >
+	<div class="container " style="margin-top: 30px;">
+		<div class="row">
+			<div class="col-md-12 ">	
+				<div class="portlet white-bg">
+					<div class="portlet-body">
+						<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentysixteen' ); ?></h1>
+						<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentysixteen' ); ?></p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12 blog-page ">	
+				<div class="portlet">
+					<?php get_sidebar(); ?>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php get_sidebar( 'content-bottom' ); ?>
+	
+<?php get_footer(); ?>
